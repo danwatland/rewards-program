72665feef058dceb21adb1ff7d7976bdfe71d6ee
Dan Watland <danwatland@gmail.com>
2023-04-07 19:46:37 -0500

    Adding loading spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import { useState } from 'react'
+import * as React from 'react'
+import { usePurchaseHistoryStore } from './state/purchase-history-store';
 import './App.css'
 
 const App = () => {
+  const { isLoading, purchaseHistory, getPurchaseHistory } = usePurchaseHistoryStore();
+
+  React.useEffect(() => {
+    const currentDate = new Date();
+    const fromDate = new Date();
+    fromDate.setMonth(currentDate.getMonth() - 3);
+
+    getPurchaseHistory(fromDate, currentDate);
+  }, []);
+
   return (
-    <div className="App">
+    <div>
+      { isLoading ? <progress /> : <progress /> }
     </div>
   )
 }
