4f52c33f013141cef48d9e54578b2f0e9eb6a09f
Dan Watland <danwatland@gmail.com>
2023-04-07 20:37:24 -0500

    Adding reward point summary

--- src/components/purchase-list-line-item.jsx
+++ src/components/purchase-list-line-item.jsx
@@ -3,12 +3,12 @@ import { calculateRewardPoints } from '../services/reward-point-calculator.js';
 
 const PurchaseListLineItem = ({ amount, date, item }) => {
   return (
-    <li className='purchase-list-line-item'>
-      <span>{item}</span>
-      <span>{date.toLocaleString()}</span>
-      <span>{amount}</span>
-      <span>{calculateRewardPoints({ amount })}</span>
-    </li>
+    <tr>
+      <td>{item}</td>
+      <td>{date.toLocaleString()}</td>
+      <td>{amount}</td>
+      <td>{calculateRewardPoints({ amount })}</td>
+    </tr>
   );
 };
 
--- src/components/purchase-list.jsx
+++ src/components/purchase-list.jsx
@@ -2,18 +2,29 @@ import './purchase-list.css';
 import * as React from 'react';
 import { usePurchaseHistoryStore } from '../state/purchase-history-store.js';
 import { PurchaseListLineItem } from './purchase-list-line-item.jsx';
+import { calculateRewardPoints } from '../services/reward-point-calculator.js';
 
 const PurchaseList = () => {
   const purchaseHistory = usePurchaseHistoryStore((state) => state.purchaseHistory);
 
   return (
     <>
-      <ul>
-        { purchaseHistory.map((purchase, i) => <PurchaseListLineItem amount={purchase.amount} date={purchase.date} item={purchase.item} key={i} />) }
-      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Date Purchased</th>
+            <th>Price</th>
+            <th>Reward Points</th>
+          </tr>
+        </thead>
+        <tbody>
+          { purchaseHistory.map((purchase, i) => <PurchaseListLineItem amount={purchase.amount} date={purchase.date} item={purchase.item} key={i} />) }
+        </tbody>
+      </table>
       <summary className='purchase-list-summary'>
         <span>Total spent in last 3 months: ${purchaseHistory.reduce((sum, purchase) => sum + purchase.amount, 0)}</span>
-        <span>Reward points earned: {'todo: make something to calculate this'}</span>
+        <span>Reward points earned: {purchaseHistory.reduce((sum, purchase) => sum + calculateRewardPoints(purchase), 0)}</span>
       </summary>
     </>
   );
